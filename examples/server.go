d892058ab27f80a08011786f2677b0332e5d424d
Ronald Schartmüller <ronald.schartmueller@gmail.com>
2021-08-13 12:38:18 +0200

    Fixed RunWithRoute example

--- examples/server.go
+++ examples/server.go
@@ -2,7 +2,9 @@ package examples
 
 import (
 	"fmt"
+	"net/http"
 
+	"github.com/go-chi/chi"
 	"github.com/schartey/dgraph-lambda-go/api"
 	"github.com/schartey/dgraph-lambda-go/lambda"
 	"github.com/schartey/dgraph-lambda-go/lambda/generated"
@@ -17,9 +19,14 @@ func RunWithServer() {
 }
 
 func RunWithRoute() {
+	r := chi.NewRouter()
+
 	resolver := &lambda.Resolver{}
 	executer := generated.NewExecuter(resolver)
 	lambda := api.New(executer)
-	err := lambda.Serve()
-	fmt.Println(err)
+
+	r.Post("/graphql-worker", lambda.Route)
+
+	fmt.Println("Lambda listening on 8686")
+	fmt.Println(http.ListenAndServe(":8686", r))
 }
