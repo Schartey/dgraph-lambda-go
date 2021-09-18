6360a2ee498629545ce19fa038ec24636c0b535e
Ronald Schartmüller <ronald.schartmueller@gmail.com>
2021-09-18 21:02:08 +0200

    Search go mod file also in parent folders.

--- internal/mod.go
+++ internal/mod.go
@@ -1,14 +1,48 @@
 package internal
 
 import (
+	"errors"
 	"os"
 	"os/exec"
+	"path/filepath"
 
 	"golang.org/x/mod/modfile"
 )
 
+func findGoModFile(root string, maxDepth int) (string, error) {
+	if root == "/" || maxDepth == 0 {
+		return "", errors.New("could not find go.mod file")
+	}
+	var files []string
+
+	err := filepath.WalkDir(root, func(path string, d os.DirEntry, err error) error {
+		files = append(files, path)
+		return nil
+	})
+	if err != nil {
+		return "", err
+	}
+	for _, file := range files {
+		if filepath.Base(file) == "go.mod" {
+			return root, nil
+		}
+	}
+	return findGoModFile(filepath.Dir(root), maxDepth-1)
+}
+
 func GetModuleName() (string, error) {
-	data, err := os.ReadFile("go.mod")
+
+	root, err := filepath.Abs(".")
+	if err != nil {
+		return "", err
+	}
+
+	root, err = findGoModFile(root, 3)
+	if err != nil {
+		return "", err
+	}
+
+	data, err := os.ReadFile(filepath.Join(root, "go.mod"))
 	if err != nil {
 		return "", err
 	}
